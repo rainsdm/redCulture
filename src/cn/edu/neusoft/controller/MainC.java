bf871132d438885d9f2ce8e0cb0ed0fd27211f66
Robert <59646708+rainsdm@users.noreply.github.com>
2025-07-04 16:51:06 +0800

    修复了不能正常退出的bug。

--- src/cn/edu/neusoft/controller/MainC.java
+++ src/cn/edu/neusoft/controller/MainC.java
@@ -11,19 +11,19 @@ public class MainC {
      * &emsp;&emsp;这个字段决定是否需要退出程序。当它等于0时，程序正常退出。
      * 否则，根据程序状态，执行对应的流程。
      */
-    private static int CURRENT_STATE = 0;
+    private int CURRENT_STATE = 0;
     /**
      * 用户已登录，应该进入已登录用户的会话页面。
      */
-    private static final int STATE_USER_LOGGED = 1;
+    public static final int STATE_USER_LOGGED = 1;
     /**
      * 用户未登录，需要注册为新用户，或者作为老用户登录。
      */
-    private static final int STATE_LOGIN_FLOW = 2;
+    public static final int STATE_LOGIN_FLOW = 2;
     /**
-     * 程序即将退出。
+     * 退出程序。
      */
-    private static final int STATE_EXIST = 0;
+    public static final int STATE_EXIST = 0;
     /**
      * 存储已登录用户的信息。在全局范围内可用。
      */
@@ -38,7 +38,6 @@ public class MainC {
      * 对外操作的真正入口。
      */
     public void mainLoop() {
-        boolean shouldLogin = true;
         while (CURRENT_STATE != STATE_EXIST) {
             switch (CURRENT_STATE) {
                 case STATE_USER_LOGGED:
@@ -61,21 +60,22 @@ public class MainC {
         UserAuthC auth = new UserAuthC();
 
         int login_menu = UserAuthView.chooseLoginMethod();
-            switch (login_menu) {
-                case 1:
-                    loggedInUser = auth.login();
+        switch (login_menu) {
+            case 1:
+                loggedInUser = auth.login();
+                if (loggedInUser != null && loggedInUser.getUser_id() != null
+                        && !loggedInUser.getUser_id().isEmpty()) {
+                    // 进行严格的登录检查。只有当它确实不为空，且变量内的ID信息也存在时，才会正常开启会话。
                     CURRENT_STATE = STATE_USER_LOGGED;
-                    break;
-                case 2:
-                    auth.register(); // 注册完成后，直接进入登录流程。
-                    break;
-                case 0:
-                    CURRENT_STATE = STATE_EXIST;
-                    break;
-            }
-        if (loggedInUser != null && loggedInUser.getUser_id() != null && !loggedInUser.getUser_id().isEmpty()) {
-            // 如果登录成功，在循环外面开启已登录用户的会话信息。
-            CURRENT_STATE = STATE_USER_LOGGED;
+                }
+                break;
+            case 2:
+                auth.register(); // 注册完成后，直接进入登录流程。
+                break;
+            case 0:
+                CURRENT_STATE = STATE_EXIST;
+                loggedInUser = null; // 退出登录后，清空已登录用户的信息。
+                break;
         }
     }
 
@@ -87,22 +87,18 @@ public class MainC {
             Scanner sc = new Scanner(System.in);
             UserCenterC userCenter = new UserCenterC();
 
-            boolean sessionActive = true;
-
-            while (sessionActive) {
-                System.out.print("请选择要进行的操作: ");
-                int manu = sc.nextInt();
-                sc.nextLine();
-                switch (manu) {
-                    case 0:
-                        sessionActive = false;
-                        System.out.println("您已退出登录。系统将回到登录界面。");
-                        CURRENT_STATE = STATE_LOGIN_FLOW;
-                        break;
-                    case 1:
-                        userCenter.managerCenter(loggedInUser);
-                        break;
-                }
+            System.out.print("请选择要进行的操作: ");
+            int manu = sc.nextInt();
+            sc.nextLine();
+            switch (manu) {
+                case 0:
+                    System.out.println("您已退出登录。系统将回到登录界面。");
+                    loggedInUser = null; // 退出登录后，清空已登录用户的信息。
+                    CURRENT_STATE = STATE_LOGIN_FLOW;
+                    break;
+                case 1:
+                    userCenter.managerCenter(loggedInUser);
+                    break;
             }
         } else if (loggedInUser != null && loggedInUser.getRole() == 0) {
             // 跳转到管理员用户的首页
