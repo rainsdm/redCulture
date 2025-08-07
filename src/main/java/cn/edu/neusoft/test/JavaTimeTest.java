ffbf885fd452d361d76080a4d3b95b8f1afbf31e
Robert <59646708+rainsdm@users.noreply.github.com>
2025-08-07 09:22:01 +0800

    第一次重构的计划。
    它仅仅是为了让系统能够适应最新修改过的数据库。将来，数据库可能还会根据需要，不断调整。
    它的主线任务就是适应数据库，不要无谓的增加突然冒出来的新功能设想，这个东西应该放在规划文件里。

---
+++ src/main/java/cn/edu/neusoft/test/JavaTimeTest.java
@@ -0,0 +1,12 @@
+package cn.edu.neusoft.test;
+
+import java.time.Instant;
+
+public class JavaTimeTest {
+
+	public static void main(String[] args) {
+		Instant testInstant = Instant.now();
+		System.out.println(testInstant);
+	}
+
+}
