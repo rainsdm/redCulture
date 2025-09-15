19afcdf768b0db7ce3ffa501ba0ada89460402f9
Robert <59646708+rainsdm@users.noreply.github.com>
2025-09-15 18:34:57 +0800

    第二次实验，但是好像有点失败。

--- src/main/java/cn/edu/neusoft/usersession/SessionDispatcher.java
+++ src/main/java/cn/edu/neusoft/usersession/SessionDispatcher.java
@@ -1,5 +1,19 @@
 package cn.edu.neusoft.usersession;
 
-public class SessionDispatcher {
+import cn.edu.neusoft.usersession.role.DispatcherStrategy;
 
+public class SessionDispatcher {
+	DispatcherStrategy dispatcher;
+	
+	public SessionDispatcher(DispatcherStrategy dispatcher) {
+		this.dispatcher = dispatcher;
+	}
+	
+	/**
+	 * 按照用户选择的角色来分发对应的策略。
+	 * @param role 0代表管理员，1代表普通用户。
+	 */
+	public void selectedRole(int role) {
+		dispatcher.recive(role);
+	}
 }
